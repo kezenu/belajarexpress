534ca5ee228520a0bb59edbf0415ed95d726e743
Wisnu Sanjaya <wsanjaya69@gmail.com>
2025-09-09 15:15:56 +0700

    vwvewqevwqvwvw

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(console.log('data'));
+});
+
+app.listen(port, () => {
+  console.log('server berjalan di port 3000');
+});
