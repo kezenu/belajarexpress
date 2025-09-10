a25c67de53eee0a9c56882f9d3d9b39d76c3c521
kezenu <wsanjaya69@gmail.com>
2025-09-10 16:23:40 +0700

    herherberbe

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const dataReverse = () => {
+  const data = 'saya adalah wisnu';
+  const split = data.split(' ');
+  const dataArray = [];
+  for (let word of split) {
+    const reversedWord = word.split('').reverse().join('');
+    dataArray.push(reversedWord);
+  }
+  return dataArray.join(' ');
+};
+
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send(dataReverse());
 });
 
 app.listen(port, () => {
