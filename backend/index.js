751b641196f451a493db886aaff90d86ec050a84
Wisnu Sanjaya <wsanjaya69@gmail.com>
2025-09-12 12:10:26 +0700

    213rf32f43f

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
 import dotenv from 'dotenv';
+
 dotenv.config();
 const app = express();
 
--- backend/models/UserModels.js
+++ backend/models/UserModels.js
@@ -1,4 +1,2 @@
 import { Sequelize, DataTypes } from 'sequelize';
 import db from '../config/Database';
-
-const Users = db.define();
